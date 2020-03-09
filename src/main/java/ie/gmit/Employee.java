36487834852b2ad66799f8d06e006e2ba70f9757
ZDENEK KROUSKY - Student <G00267758@gmit.ie>
2020-03-09 09:29:43 +0000

    Employee and EmployeeList created

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,74 @@
 package ie.gmit;
 
 public class Employee {
+
+    private String name;
+    private String phone;
+    private String id;
+    private int age;
+
+    public Employee() {
+        throw new IllegalArgumentException("Must use all the parameters");
+    }
+
+    public Employee(String name, String phone, String id, int age) {
+        if(name == null || phone == null || id == null || age == 0) {
+            throw new IllegalArgumentException("Must use all the parameters to create Employee");
+        } else {
+            this.name = name;
+            this.phone = phone;
+            this.id = id;
+            this.age = age;
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length() <= 3 || name.length() >= 25) {
+            throw new StringIndexOutOfBoundsException("Name must be between 3 and 25 characters long");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        if(phone.length() != 8) {
+            throw new IllegalArgumentException("Phone number must be 8 digits long");
+        } else {
+            this.phone = phone;
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if(id.length() != 5) {
+            throw new IllegalArgumentException("Id must be 5 digits long");
+        } else {
+            this.id = id;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 16) {
+            throw new IllegalArgumentException("Must be over 16");
+        } else
+        {
+            this.age = age;
+        }
+    }
 }
--- src/main/java/ie/gmit/EmployeeList.java
+++ src/main/java/ie/gmit/EmployeeList.java
@@ -1,4 +1,41 @@
 package ie.gmit;
 
+import java.util.ArrayList;
+
 public class EmployeeList {
+
+    private static int employeeCount = 0;
+    private ArrayList<Employee> employeeList;
+
+    public EmployeeList() {
+    }
+
+    public EmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public static int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public static void setEmployeeCount(int employeeCount) {
+        EmployeeList.employeeCount = employeeCount;
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public void addEmployee(Employee emp) {
+        this.employeeCount++;
+        this.employeeList.add(emp);
+    }
+
+    public int getListSize() {
+        return this.employeeList.size();
+    }
 }
